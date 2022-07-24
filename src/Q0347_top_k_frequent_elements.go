7035f0f993057e318324587d3db533de4155ed6d
Alwithy <zxcvgvbnm@vip.qq.com>
2022-07-24 15:43:11 +0800

    fix solution for Q0347

--- src/Q0347_top_k_frequent_elements.go
+++ src/Q0347_top_k_frequent_elements.go
@@ -29,21 +29,26 @@ func topKFrequent(nums []int, k int) []int {
 }
 
 func qsort(lists [][]int, start, end, k int) [][]int {
+	if k >= end - start + 1 {
+		return lists
+	}
 	rand.Seed(time.Now().UnixNano())
 	picked := rand.Int() % (end - start + 1) + start
 	lists[start], lists[picked] = lists[picked], lists[start]
 
 	index := start // 已筛选过的，频次小于等于当前lists[start]的数字
 	for i := start + 1; i <= end; i++ {
-		if lists[i][1] <= lists[start][1] {
+		if lists[i][1] < lists[start][1] {
 			lists[index + 1], lists[i] = lists[i], lists[index + 1]
 			index++
 		}
 	}
 	lists[index], lists[start] = lists[start], lists[index]
 
-	if end - index < k {
-		return qsort(lists, start, index, k - (end - index))
+	if end - index + 1 < k {
+		return qsort(lists, start, index - 1, k - (end - index + 1))
+	} else if end - index + 1 > k {
+		return qsort(lists, index + 1, end, k)
 	}
 	return lists
 }
